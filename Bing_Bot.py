26c193d2377da24a5dcd612de159d339e7239d71
Tienza <Piradon.Liengtiraphan1@gmail.com>
2016-11-10 09:52:56 -0500

    Script now run independent of human intervention

--- Bing_Bot.py
+++ Bing_Bot.py
@@ -1,8 +1,19 @@
 import urllib2
 import webbrowser
 import random
+import os
+import telnetlib
+import time
 from BeautifulSoup import BeautifulSoup
 
+# Debug Variable
+debug = False
+
+# Registers FireFox as available browser
+ffpath = 'C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe'
+webbrowser.register('firefox', None, webbrowser.BackgroundBrowser(ffpath), 1)
+
+# Retrieve Array of Words to use from Long Wikipedia Article
 random_words = urllib2.urlopen('https://en.wikipedia.org/wiki/1918_New_Year_Honours').read()
 clean_words = BeautifulSoup(random_words).text
 words = []
@@ -10,12 +21,51 @@ for word in clean_words.split():
     words.append(word)
 print("Length of Array of Words is " + str(len(words)))
 
+# Function to search Bing and acquire points
 def search_bing():
     word1 = random.randint(0,len(words))
     word2 = random.randint(0,len(words))
     word3 = random.randint(0,len(words))
-    webbrowser.open_new_tab('https://www.bing.com/search?q='+words[word1]+'+'+words[word2]+'+'+words[word3])
+    #webbrowser.open_new('https://www.bing.com/search?q='+words[word1]+'+'+words[word2]+'+'+words[word3])
+    open_curr_tab('https://www.bing.com/search?q='+words[word1]+'+'+words[word2]+'+'+words[word3])
     print("Done")
 
-for i in range(30):
-    search_bing()
+
+'''for i in range(30):
+    search_bing()'''
+
+# Establish TelNet Session to open Firefire so all searches are contained in one tab.
+HOST = 'localhost'
+PORT = 4242
+
+def open_curr_tab(url):
+    tn = telnetlib.Telnet(HOST, PORT)
+    cmd = "content.location.href = '{url}'".format(url=url)
+
+    tn.read_until("repl> ")
+    tn.write(cmd + "\n")
+
+    tn.write("repl.quit()\n")
+
+
+#############################################
+if debug:
+    start_time = time.time()
+    print(webbrowser._browsers)
+    webbrowser.get('firefox').open('https://www.google.com')
+    run_time = time.time() - start_time
+    minutes, seconds = divmod(run_time, 60)
+    hours, minutes = divmod(minutes, 60)
+    print "Seconds Time Format --- %s seconds ---" % (time.time() - start_time)
+    print "Normal Time Format --- %d:%02d:%02d ---" % (hours, minutes, seconds)
+
+if __name__ == "__main__":
+    webbrowser.get('firefox').open_new_tab('http://www.bing.com/')
+    time.sleep(5)
+    try:
+        for i in range(30):
+            search_bing()
+            print(str(i+1)+ " queries made...")
+        os.system("TASKKILL /F /IM firefox.exe")
+    except:
+        print("Connection refused. Please open Mozilla FireFox")
